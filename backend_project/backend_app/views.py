10f63bed88c3e3745d92cdd609dabbff21ba525a
mhoja <timotheomhoja@gmail.com>
2021-06-12 22:07:09 +0300

    Added change password

--- backend_project/backend_app/views.py
+++ backend_project/backend_app/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from rest_framework.permissions import IsAuthenticated, IsAdminUser, AllowAny
 from rest_framework.response import Response
+from rest_framework.views import APIView
 from .models import Post, Comment,PostLikes 
 from rest_framework import fields, generics, permissions, viewsets
 from django.contrib.auth.models import User
+from rest_framework_simplejwt.tokens import RefreshToken
+
 from .serializers import (
-    UserSerializerWithToken , RegisterSerializer,
+    UserSerializerWithToken , RegisterSerializer,ChangePasswordSerializer,
     PostSerializer,CommentSerializer, PostLikestSerializer)
 
 # Create your views here.
@@ -35,6 +38,54 @@ class RegisterUserView(generics.CreateAPIView):
     queryset = User.objects.all()
     permission_classes=(AllowAny,)
     serializer_class = RegisterSerializer
+
+
+
+class ChangePasswordView(generics.UpdateAPIView):
+
+    serializer_class = ChangePasswordSerializer
+    model = User
+    permission_classes = (IsAuthenticated,)
+
+    def get_object(self, queryset=None):
+        obj = self.request.user
+        return obj
+
+    def update(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        serializer = self.get_serializer(data=request.data)
+
+        if serializer.is_valid():
+            # Check old password
+            if not self.object.check_password(serializer.data.get("old_password")):
+                return Response({"old_password": ["Wrong password."]}, status=status.HTTP_400_BAD_REQUEST)
+            # set_password also hashes the password that the user will get
+            self.object.set_password(serializer.data.get("new_password"))
+            self.object.save()
+            response = {
+                'status': 'success',
+                'code': status.HTTP_200_OK,
+                'message': 'Password updated successfully',
+                'data': []
+            }
+
+            return Response(response)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class LogoutView(APIView):
+    permission_classes = (IsAuthenticated,)
+
+    def post(self, request):
+        try:
+            refresh_token = request.data["refresh_token"]
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+
+            return Response(status=status.HTTP_205_RESET_CONTENT)
+        except Exception as e:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
 
 
 # VIEW SETS
